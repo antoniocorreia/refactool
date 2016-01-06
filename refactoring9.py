3c0a872aa62e5cecad6ffb76ae216c4b44beb6b1
Antonio <antonio.alves.correia@gmail.com>
2016-01-06 00:33:47 -0300

    Refactoring 9

--- refactoring9.py
+++ refactoring9.py
@@ -1,20 +1,24 @@
 import re
 
+padrao_tipos_c = "(int|void|char|unsigned char|signed char|unsigned int|signed int|short int|unsigned short int|signed short int|long int|signed ling int|unsigned long int|float|double|long double)"
+
 def refactoring_9(codigo):
-    #para chamadas de funções a ER deve considerar todos os tipos
-    #todo: corrigir quando esta com duas funções (ex main)
-    padrao9 = re.search('(.*)(int|void|char|unsigned char|signed char|unsigned int|signed int|short int|unsigned short int|signed short int|long int|signed ling int|unsigned long int|float|double|long double) (.*?)\((.*?)#ifdef (.*?)\n(.*?)#endif(.*?)\){(.*?)}(.*)',codigo,re.DOTALL)
+    padrao9 = re.search('(.*)' + padrao_tipos_c + '(.*?)\((.*?)#ifdef (.*?)' + padrao_tipos_c +' (.*?)#endif(.*?)\)(.*?){(.*?)}(.*)',codigo,re.DOTALL)
 
     if padrao9:
         func_type = padrao9.group(2)
         func_name = padrao9.group(3)
-        param = padrao9.group(6)
-        loc_func = padrao9.group(8)
+        param_type = padrao9.group(6)
+        param_id = padrao9.group(7)
+        loc_func = padrao9.group(10)
         expression_1 = padrao9.group(5)
-        
-        codigo_transformado = "\t#ifdef " + expression_1 + "\n\t\t#define PARAM " + param + "\n\t#else\n\t\t#define PARAM \"\"\n\t#endif\n\t" + func_type + " " + func_name + "(PARAM){\n\t" + loc_func + "\n\t}\n" + padrao9.group(9)
 
-        return refactoring_9(padrao9.group(1)) + codigo_transformado
+        codigo_anterior = padrao9.group(1)
+        codigo_restante = padrao9.group(11)
+        
+        codigo_transformado = "#ifdef " + expression_1 + "\n\t#define PARAM " + param_type + " " + param_id + "\n#else\n\t#define PARAM \"\"\n#endif\n" + func_type + " " + func_name + "(PARAM){\n\t" + loc_func + "\n}\n" + codigo_restante
+
+        return refactoring_9(codigo_anterior) + codigo_transformado
     else:        
         return codigo
         
--- teste9.c
+++ teste9.c
@@ -7,35 +7,43 @@ void function(
 #endif
 ){
 	//lines of code
-}
+};
+
+void function2(
+#ifdef SIZE == 642
+	int y
+#endif
+){
+	//lines of code 2
+};
 
 main()
 {
 	int x = 1;
 	int y = 2;
-	
+
 	int id[] = {1,
 		2
 	#ifdef SIZE == 1
 		,3
 	#endif
 	};
-	
+
 	#ifdef SIZE == 1
 		if (x == 1)
 	#endif
 	{
-		x++; 
-	}
-	
-	#ifdef SIZE == 1
+		x++;
+	};
+
+	#ifdef SIZE == 64
 		if (x == 1){
 	#else
 		if (x == 2){
 	#endif
-		x++; 
+		x++;
 	}
-	
+
 	return x
 	#ifdef SIZE ==1
 		&& y
