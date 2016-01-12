d0b0bf4d64ca5d9ebc4412bcb7193759b8cd1835
CaroolSantos <a.caroolinasantos@gmail.com>
2016-01-12 04:30:22 -0200

    Update teste_e.c

--- refactool_example/teste_e.c
+++ refactool_example/teste_e.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#define SIZE 64
+#define VAR 64
 
 main()
 {
 	int x = 1;
 	int y = 2;
 
-	#ifdef SIZE == 1
+	#ifdef VAR == 1
 		if (x == 1){
 			//lines of code 1
 		}else
@@ -15,7 +15,7 @@ main()
 		//lines of code 2
 	}
 
-	#ifdef SIZE == 1
+	#ifdef VAR == 1
 		if (x == 1){
 	#else
 		if (x == 2){
@@ -23,7 +23,7 @@ main()
 		x++;
 	}
 	int z= 1;
-	#ifdef SIZE == 1
+	#ifdef VAR == 1
 		if (z == 1){
 	#else
 		if (z == 2){
