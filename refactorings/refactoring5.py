8bd4f5da0b3e83a931f8f450ab8939a4405c7507
CaroolSantos <a.caroolinasantos@gmail.com>
2016-01-12 01:21:33 -0200

    Update refactoring5.py

--- refactorings/refactoring5.py
+++ refactorings/refactoring5.py
@@ -17,12 +17,13 @@ def refactoring_5(codigo):
         loc_1 = padrao5.group(4)
         loc_2 = padrao5.group(8)
         loc = padrao5.group(9)
-
+        codigo_anterior = padrao5.group(1)
+       
         var_timestamp = "var" + str(time.time()).replace('.','')[-7:]
         
         codigo_transformado = "int " + var_timestamp + " = " + condition_1 + ";\n\t#ifdef " + expression_1 + "\n\t\tif(" + var_timestamp + "){\n" + loc_1 + "\n\t\t}\n\t\t" + var_timestamp + " = !(" + var_timestamp + ");\n\t#else\n\t\t" + var_timestamp + " = 1;\n\t#endif\n\tif(" + var_timestamp + "){\n" + loc_2 + "\n\t}\n" + loc
 
-        return refactoring_5(padrao5.group(1)) + codigo_transformado
+        return refactoring_5(codigo_anterior) + codigo_transformado
     else:        
         return codigo
         
