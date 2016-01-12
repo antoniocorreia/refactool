9d83084ff1958f4623053f4582b5c0be789f4a6c
CaroolSantos <a.caroolinasantos@gmail.com>
2016-01-12 01:23:22 -0200

    Update refactoring7.py

--- refactorings/refactoring7.py
+++ refactorings/refactoring7.py
@@ -14,10 +14,13 @@ def refactoring_7(codigo):
         id_1 = padrao7.group(2).replace('\n','').replace('\t','')
         id_2 = padrao7.group(4).replace('\n','').replace('\t','')
         id_3 = padrao7.group(6).replace('\n','').replace('\t','')
+        
+        codigo_anterior = padrao7.group(1)
+        codigo_restante = padrao7.group(8)
 
-        codigo_transformado = "#ifdef " + expression_1 + "\n\t\treturn " + id_1 + " && " + id_2 + ";\n\t#else\n\t\treturn " + id_1 + " && " + id_3 + ";\n\t#endif" + padrao7.group(8)
+        codigo_transformado = "#ifdef " + expression_1 + "\n\t\treturn " + id_1 + " && " + id_2 + ";\n\t#else\n\t\treturn " + id_1 + " && " + id_3 + ";\n\t#endif" + codigo_restante
 
-        return refactoring_7(padrao7.group(1)) + codigo_transformado
+        return refactoring_7(codigo_anterior) + codigo_transformado
     else:        
         return codigo
         
